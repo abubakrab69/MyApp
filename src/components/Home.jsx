7c1d23b8259b4c426f50d022d5f16c08dbd4ffc2
abubakrab69 <abubakr.ab69@gmail.com>
2025-12-02 17:25:33 +0500

    chnage background and added some animations

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './Navbar'
 import styles from './Home.module.css'
 
@@ -15,6 +15,11 @@ import Footer from './Footer'
 import Navbar_bottom from './Navbar_bottom'
 
 const Home = () => {
+
+  useEffect(() => {
+    document.querySelector(`.${styles.hero_section}`).classList.add(styles.show)
+  }, [])
+
   const handleDownload = () => {
     const link = document.createElement('a');
     link.href = './assets/ABUBAKR.CV.pdf';
@@ -30,18 +35,20 @@ const Home = () => {
       <div>
         {/* main section */}
         <div className={`${styles.main} ${styles.container_80}`}>
+
           {/* hero section */}
           <div className={styles.hero_section}>
-            <div className={styles.top_namimg}>
-              <h1 className={styles.main_name_text}>HELLO,</h1>
-              <h1 className={styles.main_name_text}>IT'S MUHAMMAD ABU BAKR</h1>
 
-              <h1 className={styles.hello_text}>
+            <div className={styles.top_namimg}>
+              <h1 className={`${styles.main_name_text} ${styles.fade_item}`}>HELLO,</h1>
+              <h1 className={`${styles.main_name_text} ${styles.fade_item}`}>IT'S MUHAMMAD ABU BAKR</h1>
+
+              <h1 className={`${styles.hello_text} ${styles.fade_item}`}>
                 A Mobile Application and Web Developer
               </h1>
 
               {/* social icons */}
-              <div className={styles.logo_section_social}>
+              <div className={`${styles.logo_section_social} ${styles.fade_item}`}>
                 <a href="https://www.facebook.com/profile.php?id=61554939953623" target='_blank'>
                   <div className={`${styles.first} ${styles.fb}`}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-facebook" viewBox="0 0 16 16">
@@ -49,6 +56,7 @@ const Home = () => {
                     </svg>
                   </div>
                 </a>
+
                 <a href="" target='_blank'>
                   <div className={`${styles.first} ${styles.twit}`}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-twitter" viewBox="0 0 16 16">
@@ -56,13 +64,15 @@ const Home = () => {
                     </svg>
                   </div>
                 </a>
+
                 <a href="https://www.instagram.com/maybe.bakar/" target='_blank' rel="noreferrer">
                   <div className={`${styles.first} ${styles.insta}`}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-instagram" viewBox="0 0 16 16">
-                      <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334" />
+                      <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0z" />
                     </svg>
                   </div>
                 </a>
+
                 <a href="" target='_blank'>
                   <div className={`${styles.first} ${styles.linked}`}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-linkedin" viewBox="0 0 16 16">
@@ -70,6 +80,7 @@ const Home = () => {
                     </svg>
                   </div>
                 </a>
+
                 <a href="https://github.com/abubakrab69" target='_blank'>
                   <div className={`${styles.first} ${styles.linked}`}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
@@ -77,10 +88,11 @@ const Home = () => {
                     </svg>
                   </div>
                 </a>
+
               </div>
 
               {/* buttons */}
-              <div className={styles.hero_btn}>
+              <div className={`${styles.hero_btn} ${styles.fade_item}`}>
                 <button onClick={handleDownload} className={styles.btn_cv}>
                   Grab My CV
                 </button>
@@ -93,13 +105,15 @@ const Home = () => {
             </div>
 
             {/* my image */}
-            <div className={styles.my_img}>
+            <div className={`${styles.my_img} ${styles.float_img}`}>
               <img src={MyPic} alt="MY" loading="lazy" className={styles.my_image} />
             </div>
+
           </div>
+
         </div>
 
-        {/* project section */}
+        {/* Project section */}
         <div className={`${styles.project_cover} ${styles.container_100}`}>
           <div className={`${styles.project_inner} ${styles.container_80}`}>
             <h1 className={styles.project_heading}>Projects</h1>
