d139cf6072a597b2ecb41d5651fb79de0a45dc2d
abubakrab69 <abubakr.ab69@gmail.com>
2025-12-02 15:50:29 +0500

    changing hero section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Navbar from './Navbar'
 import styles from './Home.module.css'
 
-
 import MyPic from './../assets/Mypic.png'
 import Chaidocs from './../assets/Chiadocs.png'
 import Nova from './../assets/Nova.png'
@@ -13,7 +12,6 @@ import Myntra from './../assets/myntra.png'
 
 import { Link } from 'react-router-dom'
 import Footer from './Footer'
-import { motion } from 'framer-motion'
 import Navbar_bottom from './Navbar_bottom'
 
 const Home = () => {
@@ -30,54 +28,36 @@ const Home = () => {
     <>
       <Navbar />
       <div>
-        {/* <!-- main section  --> */}
+        {/* main section */}
         <div className={`${styles.main} ${styles.container_80}`}>
-          {/* <!-- hero section  --> */}
+          {/* hero section */}
           <div className={styles.hero_section}>
-            <div
-              className={styles.top_namimg}
-              data-aos="fade-up"
-              data-aos-anchor-placement="bottom-bottom"
-              data-aos-duration="2000">
-              <motion.h1
-                className={styles.main_name_text}
-                initial={{ opacity: 0, y: -150 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.8, ease: 'easeOut' }}
-              >
-                Hello
-              </motion.h1>
+            <div className={styles.top_namimg}>
+              <h1 className={styles.main_name_text}>HELLO</h1>
+              <h1 className={styles.main_name_text}>IT'S MUHAMMAD ABU BAKR</h1>
 
-              <motion.h1
-                className={styles.hello_text}
-                initial={{ opacity: 0, y: -50 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1, delay: 0.3, ease: 'easeOut' }}
-              >
-                It's M. Abu Bakr, a Mobile App and Web Developer
-              </motion.h1>
+              <h1 className={styles.hello_text}>
+                Mobile Application and Web Developer
+              </h1>
 
               {/* social icons */}
-              <motion.div
-                className={styles.logo_section_social}
-                initial={{ opacity: 0, y: -30 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1, delay: 0.6 }}
-              >
+              <div className={styles.logo_section_social}>
                 <a href="https://www.facebook.com/profile.php?id=61554939953623" target='_blank'>
                   <div className={`${styles.first} ${styles.fb}`}>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-facebook" viewBox="0 0 16 16">
-                    <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
-                  </svg>
-                </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-facebook" viewBox="0 0 16 16">
+                      <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
+                    </svg>
+                  </div>
                 </a>
+
                 <a href="" target='_blank'>
                   <div className={`${styles.first} ${styles.twit}`}>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-twitter" viewBox="0 0 16 16">
-                    <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334q.002-.211-.006-.422A6.7 6.7 0 0 0 16 3.542a6.7 6.7 0 0 1-1.889.518 3.3 3.3 0 0 0 1.447-1.817 6.5 6.5 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.32 9.32 0 0 1-6.767-3.429 3.29 3.29 0 0 0 1.018 4.382A3.3 3.3 0 0 1 .64 6.575v.045a3.29 3.29 0 0 0 2.632 3.218 3.2 3.2 0 0 1-.865.115 3 3 0 0 1-.614-.057 3.28 3.28 0 0 0 3.067 2.277A6.6 6.6 0 0 1 .78 13.58a6 6 0 0 1-.78-.045A9.34 9.34 0 0 0 5.026 15" />
-                  </svg>
-                </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-twitter" viewBox="0 0 16 16">
+                      <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334q.002-.211-.006-.422A6.7 6.7 0 0 0 16 3.542a6.7 6.7 0 0 1-1.889.518 3.3 3.3 0 0 0 1.447-1.817 6.5 6.5 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.32 9.32 0 0 1-6.767-3.429 3.29 3.29 0 0 0 1.018 4.382A3.3 3.3 0 0 1 .64 6.575v.045a3.29 3.29 0 0 0 2.632 3.218 3.2 3.2 0 0 1-.865.115 3 3 0 0 1-.614-.057 3.28 3.28 0 0 0 3.067 2.277A6.6 6.6 0 0 1 .78 13.58a6 6 0 0 1-.78-.045A9.34 9.34 0 0 0 5.026 15" />
+                    </svg>
+                  </div>
                 </a>
+
                 <a href="https://www.instagram.com/maybe.bakar/" target='_blank' rel="noreferrer">
                   <div className={`${styles.first} ${styles.insta}`}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-instagram" viewBox="0 0 16 16">
@@ -85,67 +65,66 @@ const Home = () => {
                     </svg>
                   </div>
                 </a>
+
                 <a href="" target='_blank'>
                   <div className={`${styles.first} ${styles.linked}`}>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-linkedin" viewBox="0 0 16 16">
-                    <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
-                  </svg>
-                </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-linkedin" viewBox="0 0 16 16">
+                      <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
+                    </svg>
+                  </div>
                 </a>
+
                 <a href="https://github.com/abubakrab69" target='_blank'>
                   <div className={`${styles.first} ${styles.linked}`}>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
-  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
-</svg>
-                </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
+                      <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
+                    </svg>
+                  </div>
                 </a>
-              </motion.div>
+              </div>
 
-              {/* animated buttons */}
-              <motion.div
-                className={styles.hero_btn}
-                initial={{ opacity: 0, y: 30 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1, delay: 0.9 }}>
+              {/* buttons */}
+              <div className={styles.hero_btn}>
                 <button onClick={handleDownload} className={styles.btn_cv}>
                   Download CV
                 </button>
                 <a href="/contactus">
                   <button className={styles.btn_hire}>
-                  Hire Me
-                </button>
+                    Hire Me
+                  </button>
                 </a>
-              </motion.div>
+              </div>
             </div>
 
-            <motion.div
-              className={styles.my_img}
-              initial={{ opacity: 0, scale: 0.8 }}
-              animate={{ opacity: 1, scale: 1 }}
-              transition={{ duration: 1, delay: 1 }}
-            >
+            {/* my image */}
+            <div className={styles.my_img}>
               <img src={MyPic} alt="MY" loading="lazy" className={styles.my_image} />
-            </motion.div>
+            </div>
           </div>
         </div>
+
         {/* project section */}
         <div className={`${styles.project_cover} ${styles.container_100}`}>
           <div className={`${styles.project_inner} ${styles.container_80}`}>
             <h1 className={styles.project_heading}>Projects</h1>
+
             <div className={styles.project_container}>
+
               <div className={styles.project_first_line}>
                 <a rel="noreferrer" href="https://chai-code-abubakr.netlify.app" className={styles.outer_link} target="_blank">
-                  <div className={styles.project1} data-aos="zoom-in-right">
+                  <div className={styles.project1}>
                     <img src={Chaidocs} alt="Chaidocs" loading="lazy" className={styles.proj_img} width="100%" />
                   </div>
                 </a>
+
                 <a rel="noreferrer" href="https://nova-abubakr.netlify.app" className={styles.outer_link} target="_blank">
-                  <div className={styles.project1} data-aos="zoom-in">
+                  <div className={styles.project1}>
                     <img src={Nova} alt="Nova" className={styles.proj_img} loading="lazy" width="100%" />
                   </div>
                 </a>
+
                 <a rel="noreferrer" href="https://skill-grow-abubakr.netlify.app" className={styles.outer_link} target="_blank">
-                  <div className={styles.project1} data-aos="zoom-in-left">
+                  <div className={styles.project1}>
                     <img src={Skillgrow} alt="Skill Grow" className={styles.proj_img} loading="lazy" width="100%" />
                   </div>
                 </a>
@@ -153,17 +132,19 @@ const Home = () => {
 
               <div className={styles.project_first_line}>
                 <a rel="noreferrer" href="https://i-coder-abubakr.netlify.app" className={styles.outer_link} target="_blank">
-                  <div className={styles.project1} data-aos="flip-down">
+                  <div className={styles.project1}>
                     <img src={icoder} alt="iCoder" loading="lazy" className={styles.proj_img} width="100%" />
                   </div>
                 </a>
+
                 <a rel="noreferrer" href="https://hurst-abubakr.netlify.app" className={styles.outer_link} target="_blank">
-                  <div className={styles.project1} data-aos="zoom-in">
+                  <div className={styles.project1}>
                     <img src={Hurst} alt="Hurst" className={styles.proj_img} loading="lazy" width="100%" />
                   </div>
                 </a>
+
                 <a rel="noreferrer" href="https://mintra-abubakr.netlify.app" className={styles.outer_link} target="_blank">
-                  <div className={styles.project1} data-aos="flip-down">
+                  <div className={styles.project1}>
                     <img src={Myntra} alt="Myntra" loading="lazy" className={styles.proj_img} width="100%" />
                   </div>
                 </a>
@@ -174,6 +155,7 @@ const Home = () => {
               </Link>
             </div>
           </div>
+
           <Navbar_bottom />
           <Footer />
         </div>
