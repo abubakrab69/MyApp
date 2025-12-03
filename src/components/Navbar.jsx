57cf03f30f334598d205c723d34e33698f00e5b8
abubakrab69 <abubakr.ab69@gmail.com>
2025-12-03 13:21:28 +0500

    testing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
           <ul className='nav_links'>
           <li className='link'>
             <NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>
-              Home
+              Hom
             </NavLink>
           </li>
           <li className='link'>
