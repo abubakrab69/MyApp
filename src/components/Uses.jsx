c1b1a6a96e08b208dae6b37e6fa1f08e15ee5b39
abubakrab69 <abubakr.ab69@gmail.com>
2025-12-04 11:34:46 +0500

    `Updated Project.module.css and Uses.jsx with new styles and layout changes`

--- src/components/Uses.jsx
+++ src/components/Uses.jsx
@@ -13,7 +13,26 @@ const Uses = () => {
       <Navbar />
       <div className={styles.uses_main_container}>
         <img src={Laptop} alt="" className={styles.laptop_image}/>
+        <div>
+          <h1 className={styles.laptopName}>Dell Letitude 5420 14''</h1>
+        </div>
+        <div className={styles.spesContainer}>
+           <div className={styles.specBox}>
+            16 GB
+          </div>
+         <div className={styles.specBox}>
+            256 GB
+          </div>
+         <div className={styles.specBox}>
+            11th Generation
+          </div>
+         <div className={styles.specBox}>
+            Core i5
+          </div>
+        </div>
       </div>
+
+      
       <Navbar_bottom/>
       <Footer />
     </>
