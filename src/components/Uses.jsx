6589dc080c3cbc67532f0234d17221e695670f22
abubakrab69 <abubakr.ab69@gmail.com>
2025-12-04 19:02:08 +0500

    msjne

--- src/components/Uses.jsx
+++ src/components/Uses.jsx
@@ -6,6 +6,16 @@ import { motion } from 'framer-motion'
 
 import Navbar_bottom from './Navbar_bottom'
 import Laptop from './../assets/laptop1.png'
+import DART from './../assets/dart.png'
+import Github from './../assets/github.png'
+import Git from './../assets/git.png'
+import Postman from './../assets/postman.png'
+import Slack from './../assets/slack.png'
+import Vercel from './../assets/vercel.webp'
+import AndriodS from './../assets/androidstudio.png'
+import NPM from './../assets/npm.webp'
+import VSCode from './../assets/vscode.png'
+import Emulator from './../assets/emulator.png'
 
 const Uses = () => {
   return (
@@ -31,6 +41,42 @@ const Uses = () => {
           </div>
          <div className={styles.specBox}>
             Windows 11 Pro
+          </div>
+        </div>
+        <h1 className={styles.laptopName}>Tools</h1>
+        <div className={styles.tools_main_container}>
+          <div className={styles.tool_one}>
+            <img src={Postman} alt="" className={styles.tools_images}/>
+          </div>
+          <div className={styles.tool_one}>
+            <img src={Vercel} alt="" className={styles.tools_images}/>
+          </div>
+          <div className={styles.tool_one}>
+            <img src={Slack} alt="" className={styles.tools_images}/>
+          </div>
+          <div className={styles.tool_one}>
+            <img src={DART} alt="" className={styles.tools_images}/>
+          </div>
+          <div className={styles.tool_one}>
+            <img src={AndriodS} alt="" className={styles.tools_images}/>
+          </div>
+          <div className={styles.tool_one}>
+            <img src={VSCode} alt="" className={styles.tools_images}/>
+          </div>
+          <div className={styles.tool_one}>
+            <img src={DART} alt="" className={styles.tools_images}/>
+          </div>
+          <div className={styles.tool_one}>
+            <img src={Emulator} alt="" className={styles.tools_images}/>
+          </div>
+          <div className={styles.tool_one}>
+            <img src={NPM} alt="" className={styles.tools_images}/>
+          </div>
+          <div className={styles.tool_one}>
+            <img src={Git} alt="" className={styles.tools_images}/>
+          </div>
+          <div className={styles.tool_one}>
+            <img src={Github} alt="" className={styles.tools_images}/>
           </div>
         </div>
       </div>
