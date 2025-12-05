b58b86857824398858d398f3e17ebdcc9a450795
abubakrab69 <abubakr.ab69@gmail.com>
2025-12-05 18:31:54 +0500

    `Removed import of motion from framer-motion`

--- src/components/Uses.jsx
+++ src/components/Uses.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Navbar from './Navbar'
 import styles from './Uses.module.css'
 import Footer from './Footer'
-import { motion } from 'framer-motion'
 
 import Navbar_bottom from './Navbar_bottom'
 import Laptop from './../assets/laptop1.png'
