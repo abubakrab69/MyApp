26220ac99d5a5b95b62636bd54fbb9888147e7e0
abubakrab69 <abubakr.ab69@gmail.com>
2025-12-08 12:58:21 +0500

    `No changes in code, import statement remains the same`

--- src/splash/Splash.jsx
+++ src/splash/Splash.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import AnimatedStarfield from "../animations/AnimatedStarfield"; // adjust path
+import AnimatedStarfield from "../animations/AnimatedStarfield"; 
 import Logo from './../assets/logo_main.png'
 
 const Splash = () => {
