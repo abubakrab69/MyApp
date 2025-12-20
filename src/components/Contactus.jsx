c6be9bb61e1c935b4b8b0f9c1d4c9d3460e15f6a
abubakrab69 <abubakr.ab69@gmail.com>
2025-12-20 21:35:30 +0500

    `Changed JSX wrapper from <> to <>`

--- src/components/Contactus.jsx
+++ src/components/Contactus.jsx
@@ -24,7 +24,7 @@ const Contactus = () => {
     };
 
     return (
-        <>
+        <> 
             <Navbar />
             <div className={styles.contact_main}>
                 <div className={styles.logos}>
