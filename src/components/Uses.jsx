d9c00b7e8999f77dba11bd149502d66995f83a53
abubakrab69 <abubakr.ab69@gmail.com>
2025-12-28 16:06:24 +0500

    `Removed trailing whitespace in Uses.jsx component`

--- src/components/Uses.jsx
+++ src/components/Uses.jsx
@@ -79,7 +79,7 @@ const Uses = () => {
                 </div>
               ))}
             </div>
-          </div> 
+          </div>  
           {/* Tools Section */}
           <div className={styles.toolsSection}>
             <div className={styles.sectionHeader}>
