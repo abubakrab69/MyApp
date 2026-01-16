4268f63b1608c87a9427a0e0ce6a85c6ce281280
abubakrab69 <abubakr.ab69@gmail.com>
2026-01-16 16:48:43 +0500

    asdfg

--- src/components/Uses.jsx
+++ src/components/Uses.jsx
@@ -81,7 +81,7 @@ const Uses = () => {
               ))}
             </div> 
           </div>  
-          
+
           {/* Tools Section */}
           <div className={styles.toolsSection}>
             <div className={styles.sectionHeader}>
@@ -110,6 +110,7 @@ const Uses = () => {
           </div>
         </div>
       </div>
+      
       <Navbar_bottom />  
       <Footer />
     </>
